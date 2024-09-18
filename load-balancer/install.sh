37962fb6dc96f326bf385343ca4f379d8c8acb6f
Julio Sene <julio.sene@gmail.com>
2024-09-18 19:16:56 -0300

    modified:   load-balancer/install.sh 	modified:   load-balancer/keepalived.conf

--- load-balancer/install.sh
+++ load-balancer/install.sh
@@ -1,6 +1,8 @@
 echo "Installing Keepalived, Nginx and Nginx UI..."
-echo "Please, what will be your VIP/CIR? This is the IP address shared between all servers as a single entry point. (ex: 192.168.1.10/24)"
-read -p "VIP/CIR: " vip_cir
+echo "Please, what will be your VIP? This is the IP address shared between all servers as a single entry point. (ex: 192.168.1.10)"
+read -p "VIP: " vip
+echo "What will be your network CIR (netmask)?  (ex: when your network is somthing like 192.168.1.10/24 your CIR is 24)"
+read -p "VIP: " cir
 echo "Now, inform the IPs of your other keepalived servers that will share the same VIP. Do not add this machine IP to the list. Comma separated (ex: 192.168.1.22, 192.168.1.23)"
 read -p "servers IPs: " ips_list
 
@@ -46,7 +48,8 @@ sed -i "s/#PASSWD#/$password/g" ~/keepalived.conf
 sed -i "s/#STATE#/$state/g" ~/keepalived.conf
 sed -i "s/#MYIP#/$myip/g" ~/keepalived.conf
 sed -i "s/#SERVERSIPS#/$ips_servers/g" ~/keepalived.conf
-sed -i "s/#VIPCIR#/$vip_cir/g" ~/keepalived.conf
+sed -i "s/#VIP#/$vip/g" ~/keepalived.conf
+sed -i "s/#CIR#/$cir/g" ~/keepalived.conf
 sed -i "s/#PRIORITY#/$priority/g" ~/keepalived.conf
 sed -i "s/#INTERFACE#/$interface/g" ~/keepalived.conf
  
