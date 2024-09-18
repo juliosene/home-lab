e1fe130bd2e5e7fb36d43f8e21254e8fa62cd9fa
Julio Sene <julio.sene@gmail.com>
2024-09-18 16:33:02 -0300

    modified:   load-balancer/install.sh 	new file:   load-balancer/keepalived.conf

--- load-balancer/install.sh
+++ load-balancer/install.sh
@@ -1,4 +1,20 @@
 echo "Installing Keepalived, Nginx and Nginx UI..."
+echo "Please, what will be your VIP/CIR? This is the IP address shared between all servers as a single entry point. (ex: 192.168.1.10/24)"
+read -p "VIP/CIR: " vip_cir
+echo "Now, inform the IPs of your other keepalived servers that will share the same VIP. Do not add this machine IP to the list. Comma separated (ex: 192.168.1.22, 192.168.1.23)"
+read -p "servers IPs: " ips_list
+
+myip=$(hostname -I | awk '{print $1}')
+
+password=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 8; echo)
+
+echo "will this machine be the main node (master)?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) state="MASTER";;
+        No ) state="SLAVE";;
+    esac
+done
 
 apt update
 apt upgrade -y
@@ -6,6 +22,25 @@ apt upgrade -y
 # Install keepalived
 echo "Installing Keepalived..."
 apt install keepalived -y
+
+# Configuring keepalived
+
+ips_servers=""
+IFS=','; for word in $ips_list; do ips_servers+=${word}"\n"; done
+
+echo -e $ips_servers| tr -d ' '
+
+curl -fsSL https://raw.githubusercontent.com/juliosene/home-lab/main/load-balancer/keepalived.conf > ~/keepalived.conf
+
+sed -i "s/#PASSWD#/$password/g" ~/keepalived.conf
+sed -i "s/#STATE#/$state/g" ~/keepalived.conf
+sed -i "s/#MYIP#/$myip/g" ~/keepalived.conf
+sed -i "s/#SERVERSIPS#/$ips_servers/g" ~/keepalived.conf
+sed -i "s/#VIPCIR#/$vip_cirs/g" ~/keepalived.conf
+
+mv ~/keepalived.conf /etc/keepalived/keepalived.conf
+
+service keepalived restart
 
 # Install Nginx
 echo "Installing Nginx..."
