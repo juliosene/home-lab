59494e483b4843128e7517a280f527db8314bbc7
Julio Sene <julio.sene@gmail.com>
2024-11-08 14:52:41 -0300

    modified:   docker-swarm/install.sh

--- docker-swarm/install.sh
+++ docker-swarm/install.sh
@@ -1,36 +1,74 @@
+#!/bin/bash
 echo "Installing Docker Swarm..."
 echo ""
 
-
 MANAGER_IP=${1:-0}
-#MANAGER_IP=0
 TOKEN=${2:-0}
-#USER=${3:'docker'}
 DKR_USER="docker"
 
+IS_SWARM=false
+IS_MASTER=false
 
-if [ $MANAGER_IP == 0 ]
-then
-    MANAGER_IP=$(hostname -I | awk '{print $1}')
-#    echo "Please, what will be your manager IP? This is the IP address used by Swarm master node. (ex: 192.168.1.10)"
-#    read -p "Manager IP: " MANAGER_IP
+# Check if Docker is installed
+if command -v docker &> /dev/null; then
+    read -p "Docker is already installed. Do you want to proceed with the installation and configuration? (yes/no): " PROCEED
+    if [ "$PROCEED" != "yes" ]; then
+        echo "Installation aborted."
+        exit 0
+    fi
+else
+    echo "Docker is not installed. Proceeding with installation."
+fi
+
+# Check if Swarm is active
+SWARM_STATUS=$(docker info --format '{{.Swarm.LocalNodeState}}')
+if [ "$SWARM_STATUS" == "active" ]; then
+    echo "Docker Swarm is already active on this machine."
+    # Check if the machine is a manager or worker
+    NODE_ROLE=$(docker info --format '{{.Swarm.ControlAvailable}}')
+    if [ "$NODE_ROLE" == "true" ]; then
+        echo "This machine is a manager node in the Docker Swarm."
+        IS_MASTER=true
+    else
+        echo "This machine is a worker node in the Docker Swarm."
+        IS_MASTER=false
+    fi
+    
+    # Print messages for adding other workers and managers if the machine is a manager node
+    if [ "$IS_MASTER" == "true" ]; then
+        MANAGER_IP=$(hostname -I | awk '{print $1}')
+        echo ""
+        echo "To add swarm worker nodes, use the command:"
+        echo ""
+        echo "bash install.sh $MANAGER_IP:2377 $(docker swarm join-token -q worker)"
+        echo ""
+        echo "To add swarm manager nodes, use the command:"
+        echo ""
+        echo "bash install.sh $MANAGER_IP:2377 $(docker swarm join-token -q manager)"
+        echo ""
+    fi
+    
+    exit 0
 fi
 
 
-# Run the following command to uninstall all conflicting packages:
+if [ $MANAGER_IP == 0 ] && [ "$SWARM_STATUS" == "inactive" ]; then
+    MANAGER_IP=$(hostname -I | awk '{print $1}')
+fi
+
+# Run the following command to uninstall all conflicting packages
 apt update
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 apt upgrade -y
 
-
-# Add Docker's official GPG key:
+# Add Docker's official GPG key
 sudo apt-get update
-sudo apt-get install  -y ca-certificates curl
+sudo apt-get install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# Add the repository to Apt sources:
+# Add the repository to Apt sources
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
@@ -39,53 +77,52 @@ sudo apt-get update
 
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-# Testing docker...
+# Test Docker
 sudo docker run hello-world
 
-# post-installation
+# Post-installation
 
-# Add user to docker
+# Add user to Docker
 sudo adduser --disabled-password --gecos "" $DKR_USER
 
-# Create the docker group.
+# Create the Docker group
 sudo groupadd docker
 
-# Add your user to the docker group.
+# Add your user to the Docker group
 sudo usermod -aG docker $DKR_USER
 
 echo "" > /home/"$DKR_USER"/.docker
 sudo chown "$DKR_USER":"$DKR_USER" /home/"$DKR_USER"/.docker -R
 sudo chmod g+rwx "/home/"$DKR_USER"/.docker" -R
 
-
-# Verify that you can run docker commands without sudo.
+# Verify that you can run Docker commands without sudo
 docker run hello-world
 
 # Configure Docker to start on boot with systemd
 sudo systemctl enable docker.service
 sudo systemctl enable containerd.service
 
-
-# SWARM
+# Swarm
 
 # Example iptables rule (order and other tools may require customization)
 # iptables -I INPUT -m udp --dport 4789 -m policy --dir in --pol none -j DROP
 
-#  create a new swarm
+# Create a new swarm
+if [ $TOKEN == 0 ]; then
+  # Create a swarm on the manager machine
+  docker swarm init --advertise-addr $MANAGER_IP
 
-if [ $TOKEN == 0 ]
-then
-  # creates a swarm on the manager machine
-  OUTPUT=$(docker swarm init --advertise-addr $MANAGER_IP)
-  # find the token and IP:port for other nodes
-  for word in $OUTPUT; do if [[ $next == 2 ]]; then TOKEN=$word;((next--)); else if [[ $next == 1 ]]; then IP_PORT=$word;((next--)); fi; fi;  if [ $word == "--token" ]; then next=2; fi; done
+   # Check if Swarm is active
+   SWARM_STATUS=$(docker info --format '{{.Swarm.LocalNodeState}}')
+   if [ "$SWARM_STATUS" == "active" ]; then
 
-  #install portainer
+
+  # Install Portainer
   curl -L https://downloads.portainer.io/ce2-21/portainer-agent-stack.yml -o portainer-agent-stack.yml
   docker stack deploy -c portainer-agent-stack.yml portainer
 else
-  # add the machine to swarm cluster as a worker
-  sudo  docker swarm join --token $TOKEN $MANAGER_IP
+  # Add the machine to swarm cluster as a worker
+  sudo docker swarm join --token $TOKEN $MANAGER_IP
   IP_PORT=$MANAGER_IP
 fi
 
@@ -93,7 +130,11 @@ docker info
 docker node ls
 
 echo ""
-echo "To install swarm worker nodes, use the command:"
+echo "To add swarm worker nodes, use the command:"
 echo ""
-echo "bash install.sh $IP_PORT $TOKEN"
+echo "bash install.sh $MANAGER_IP:2377 $(docker swarm join-token -q worker)"
+echo ""
+echo "To add swarm manager nodes, use the command:"
+echo ""
+echo "bash install.sh $MANAGER_IP:2377 $(docker swarm join-token -q manager)"
 echo ""
