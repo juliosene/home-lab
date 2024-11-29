cff53ded9890b554b0c1c1263c3f6748f4659829
Julio Sene <julio.sene@gmail.com>
2024-11-29 11:10:38 -0300

    modified:   docker-swarm/install-beta.sh

--- docker-swarm/install-beta.sh
+++ docker-swarm/install-beta.sh
@@ -4,6 +4,13 @@
 # URL for the install script
 INSTALL_SCRIPT_URL="https://install.cluster4.me"
 
+MANAGER_IP=${1:-0}
+TOKEN=${2:-0}
+DKR_USER="docker"
+
+IS_SWARM=false
+IS_MASTER=false
+
 # Function to print banners
 print_banner() {
     echo ""
@@ -12,15 +19,13 @@ print_banner() {
     echo "=========================================="
     echo ""
 }
+print_minibanner() {
+    echo ""
+    echo "=== $1 ==="
+    echo ""
+}
 
 print_banner "Starting install.Cluster4.me"
-
-MANAGER_IP=${1:-0}
-TOKEN=${2:-0}
-DKR_USER="docker"
-
-IS_SWARM=false
-IS_MASTER=false
 
 # Detect the OS
 if [ -f /etc/os-release ]; then
@@ -29,7 +34,7 @@ if [ -f /etc/os-release ]; then
     VERSION=$VERSION_ID
 else
     print_banner "   ATTENTION!"
-    echo "Cannot detect the operating system. Exiting."
+    print_minibanner "Cannot detect the operating system. Exiting."
     exit 1
 fi
 
@@ -38,11 +43,11 @@ if command -v docker &> /dev/null; then
     print_banner "    ATTENTION!"
     read -p "Docker is already installed. Do you want to proceed with the installation and configuration? (yes/no): " PROCEED
     if [ "$PROCEED" != "yes" ]; then
-        echo "Installation aborted."
+        print_minibanner "Installation aborted."
         exit 0
     fi
 else
-    echo "Docker is not installed. Proceeding with installation."
+    print_minibanner "Docker is not installed. Proceeding with installation."
 fi
 
 # Function to install Docker on Debian-based systems
@@ -109,74 +114,27 @@ configure_firewall_amazon() {
     sudo service iptables restart
 }
 
+# Function to check and enable sysctl parameters
+enable_sysctl_param() {
+    local param_name="$1"
+    local param_value="$2"
 
-# Install Docker based on the detected OS
-if [[ "$OS" == "ubuntu" || "$OS" == "debian" ]]; then
-    install_docker_debian
-    configure_firewall_ubuntu
-elif [[ "$OS" == "rocky" || "$OS" == "centos" ]]; then
-    install_docker_rhel
-    configure_firewall
-elif [[ "$OS" == "amzn" ]]; then
- #   configure_firewall_amazon
-    install_docker_amazon
-else
-    print_banner "    ATTENTION!"
-    echo "Unsupported operating system: $OS. Attempting to proceed based on package manager detection."
-
-    # Check if the package manager is apt (Debian-based)
-    if command -v apt &> /dev/null; then
-        echo "Detected apt package manager. Proceeding with Debian-based installation."
-        install_docker_debian
-
-    # Check if the package manager is dnf or yum (RHEL-based)
-    elif command -v dnf &> /dev/null || command -v yum &> /dev/null; then
-        echo "Detected dnf/yum package manager. Proceeding with RHEL-based installation."
-        install_docker_rhel
-        configure_firewall
+    if [ "$(sysctl -n $param_name)" -ne "$param_value" ]; then
+        print_minibanner "$param_name = $param_value" | sudo tee -a /etc/sysctl.conf
+        sudo sysctl -w "$param_name=$param_value"
+        print_minibanner "$param_name is now set to $param_value"
     else
-        print_banner "   ATTENTION!"
-        echo "Unsupported package manager. Exiting."
-        exit 1
+        print_minibanner "$param_name is already set to $param_value"
     fi
-fi
+}
 
-# Check if Swarm is active
-SWARM_STATUS=$(sudo docker info --format '{{.Swarm.LocalNodeState}}')
-if [ "$SWARM_STATUS" == "active" ]; then
-    print_banner "   ATTENTION!"
-    echo "Docker Swarm is already active on this machine."
-    NODE_ROLE=$(sudo docker info --format '{{.Swarm.ControlAvailable}}')
-    if [ "$NODE_ROLE" == "true" ]; then
-        echo "This machine is a manager node in the Docker Swarm."
-        IS_MASTER=true
-    else
-        echo "This machine is a worker node in the Docker Swarm."
-        IS_MASTER=false
-    fi
-    
-    if [ "$IS_MASTER" == "true" ]; then
-        MANAGER_IP=$(hostname -I | awk '{print $1}')
-        echo ""
-        echo "To add swarm manager nodes, use the command:"
-        echo ""
-        echo "wget $INSTALL_SCRIPT_URL -O install.sh"
-        echo "bash install.sh $MANAGER_IP:2377 $(sudo docker swarm join-token -q manager)"
-        echo ""
-        echo "To add swarm worker nodes, use the command:"
-        echo ""
-        echo "wget $INSTALL_SCRIPT_URL -O install.sh"
-        echo "bash install.sh $MANAGER_IP:2377 $(sudo docker swarm join-token -q worker)"
-        echo ""
-    fi
-    
-    exit 0
-fi
-
-print_banner "Updating system..."
+# Capturing the machine's IP address
 if [ "$MANAGER_IP" == "0" ]; then
     MANAGER_IP=$(hostname -I | awk '{print $1}')
 fi
+
+########################################################################################
+print_banner "Updating system and remove old Docker..."
 
 # Run the following command to uninstall all conflicting packages
 sudo timedatectl set-ntp true
@@ -200,20 +158,8 @@ else
     fi
 fi
 
+########################################################################################
 print_banner "Configuring network settings..."
-# Function to check and enable sysctl parameters
-enable_sysctl_param() {
-    local param_name="$1"
-    local param_value="$2"
-
-    if [ "$(sysctl -n $param_name)" -ne "$param_value" ]; then
-        echo "$param_name = $param_value" | sudo tee -a /etc/sysctl.conf
-        sudo sysctl -w "$param_name=$param_value"
-        echo "$param_name is now set to $param_value"
-    else
-        echo "$param_name is already set to $param_value"
-    fi
-}
 
 # Check and enable bridge-nf-call-iptables
 enable_sysctl_param "net.bridge.bridge-nf-call-iptables" 1
@@ -224,9 +170,78 @@ enable_sysctl_param "net.bridge.bridge-nf-call-ip6tables" 1
 # Apply the changes
 sudo sysctl --system
 
-echo "All network changes have been applied."
+print_minibanner "All network changes have been applied."
 
+########################################################################################
+print_banner "Installing Docker..."
+
+# Install Docker based on the detected OS
+if [[ "$OS" == "ubuntu" || "$OS" == "debian" ]]; then
+    install_docker_debian
+    configure_firewall_ubuntu
+elif [[ "$OS" == "rocky" || "$OS" == "centos" ]]; then
+    install_docker_rhel
+    configure_firewall
+elif [[ "$OS" == "amzn" ]]; then
+ #   configure_firewall_amazon
+    install_docker_amazon
+else
+    print_banner "    ATTENTION!"
+    print_minibanner "Unsupported operating system: $OS. Attempting to proceed based on package manager detection."
+
+    # Check if the package manager is apt (Debian-based)
+    if command -v apt &> /dev/null; then
+        print_minibanner "Detected apt package manager. Proceeding with Debian-based installation."
+        install_docker_debian
+
+    # Check if the package manager is dnf or yum (RHEL-based)
+    elif command -v dnf &> /dev/null || command -v yum &> /dev/null; then
+        print_minibanner "Detected dnf/yum package manager. Proceeding with RHEL-based installation."
+        install_docker_rhel
+        configure_firewall
+    else
+        print_banner "   ATTENTION!"
+        print_minibanner "Unsupported package manager. Exiting."
+        exit 1
+    fi
+fi
+
+########################################################################################
 print_banner "Setting up Docker Swarm..."
+
+# Check if Swarm is active
+SWARM_STATUS=$(sudo docker info --format '{{.Swarm.LocalNodeState}}')
+if [ "$SWARM_STATUS" == "active" ]; then
+    print_banner "   ATTENTION!"
+    print_minibanner "Docker Swarm is already active on this machine."
+    NODE_ROLE=$(sudo docker info --format '{{.Swarm.ControlAvailable}}')
+    if [ "$NODE_ROLE" == "true" ]; then
+        print_minibanner "This machine is a manager node in the Docker Swarm."
+        IS_MASTER=true
+    else
+        print_minibanner "This machine is a worker node in the Docker Swarm."
+        IS_MASTER=false
+    fi
+    
+    if [ "$IS_MASTER" == "true" ]; then
+        MANAGER_IP=$(hostname -I | awk '{print $1}')
+        echo ""
+        echo "To add swarm manager nodes, use the command:"
+        echo ""
+        echo "wget $INSTALL_SCRIPT_URL -O install.sh"
+        echo "bash install.sh $MANAGER_IP:2377 $(sudo docker swarm join-token -q manager)"
+        echo ""
+        echo "To add swarm worker nodes, use the command:"
+        echo ""
+        echo "wget $INSTALL_SCRIPT_URL -O install.sh"
+        echo "bash install.sh $MANAGER_IP:2377 $(sudo docker swarm join-token -q worker)"
+        echo ""
+    fi
+    
+    exit 0
+fi
+
+
 # Create a new swarm
 if [ $TOKEN == 0 ]; then
     sudo docker swarm init --advertise-addr $MANAGER_IP
@@ -241,15 +256,24 @@ else
     sudo docker swarm join --token $TOKEN $MANAGER_IP
 fi
 
-print_banner "Final check: Docker Info"
-sudo docker info
+########################################################################################
+print_banner "Final check:"
 
+SWARM_STATUS=$(sudo docker info --format '{{.Swarm.LocalNodeState}}')
+if [ "$SWARM_STATUS" == "active" ]; then
+    sudo docker info
+    sudo docker node ls
+else
+    print_minibanner "Something went wrong. Exiting..."
+    exit 0
+if
+
+########################################################################################
 print_banner "Docker Swarm up and running!"
 
 NODE_ROLE=$(sudo docker info --format '{{.Swarm.ControlAvailable}}')
 if [ "$NODE_ROLE" == "true" ]; then
-    docker node ls
-
+    
     echo ""
     echo "To add swarm manager nodes, use the command:"
     echo ""
